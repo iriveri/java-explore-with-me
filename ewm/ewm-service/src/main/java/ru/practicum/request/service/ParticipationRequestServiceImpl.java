50cf81f9851110da64704b94a1c461580162cff9
Нестеренко Александр Владимирович <empty@empty>
2024-08-07 18:35:01 +0300

    fix: Комит для сравнения ошибок

--- ewm/ewm-service/src/main/java/ru/practicum/request/service/ParticipationRequestServiceImpl.java
+++ ewm/ewm-service/src/main/java/ru/practicum/request/service/ParticipationRequestServiceImpl.java
@@ -57,10 +57,10 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
         }
 
         // Проверка на лимит запросов на участие
-//        if (event.getParticipantLimit() != null && event.getParticipantLimit() != 0 &&
-//                repo.countByEventId(eventId) >= event.getParticipantLimit()) {
-//            throw new ConditionNotMetException("Participant limit reached.");
-//        }
+        if (event.getParticipantLimit() != null && event.getParticipantLimit() != 0 &&
+                repo.countByEventId(eventId) >= event.getParticipantLimit()) {
+            throw new ConditionNotMetException("Participant limit reached.");
+        }
 
         ParticipationRequest participationRequest = new ParticipationRequest();
         participationRequest.setParticipant(user);
@@ -68,7 +68,7 @@ public class ParticipationRequestServiceImpl implements ParticipationRequestServ
         participationRequest.setCreated(LocalDateTime.now());
 
         // Если пре-модерация отключена, запрос автоматически подтвержден
-        if (!event.getRequestModeration()) {
+        if (!event.getRequestModeration() || event.getParticipantLimit() == 0) {
             participationRequest.setStatus(RequestStatus.CONFIRMED);
         } else {
             participationRequest.setStatus(RequestStatus.PENDING);
